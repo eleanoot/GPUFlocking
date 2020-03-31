934989776065405613f99345bf9e5362f61d0d2d
Eleanor <gregoryeleanor@yahoo.co.uk>
2020-03-31 11:37:17 +0100

    Obstacle speed slowed

--- CSC8503/GameTech/Obstacle.cpp
+++ CSC8503/GameTech/Obstacle.cpp
@@ -6,5 +6,5 @@ void Obstacle::UpdateObstacle(float dt)
 {
 	Vector3 newPos = startPos + Vector3(0, 0, sin(count) * 50);
 	renderObject->GetTransform()->SetWorldPosition(newPos);
-	count += 0.1;
+	count += 0.01;
 }
