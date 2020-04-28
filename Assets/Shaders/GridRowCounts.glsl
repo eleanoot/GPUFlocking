0adcc051a46755f5f252d7be47a050962fc778c6
eleanoot <gregoryeleanor@yahoo.co.uk>
2020-04-28 12:52:48 +0100

    Buffers filling up with some kind of offset

--- Assets/Shaders/GridRowCounts.glsl
+++ Assets/Shaders/GridRowCounts.glsl
@@ -17,12 +17,12 @@ layout(std430, binding = 9) buffer GridRowCounts
 	uint row_counts[];
 };
 
-layout(local_size_x = 128, local_size_y = 1, local_size_z = 1) in; /// ???
+layout(local_size_x = 25, local_size_y = 1, local_size_z = 1) in; /// ???
 
 void main()
 {
 	// go through a full row of cells in a for loop per thread 
-	uint i = gl_LocalInvocationID.x * cellCounts.x; // index to start from
+	uint i = gl_LocalInvocationID.x; // index to start from
 	uint last = i + cellCounts.x;
 
 	uint rollingOffset = 0;
@@ -32,6 +32,5 @@ void main()
 		rollingOffset += boid_counts[i];
 	}
 
-	//atomicAdd(row_counts[i], rollingOffset);
-	row_counts[i] = 5;
+	row_counts[gl_LocalInvocationID.x] = rollingOffset;
 }
